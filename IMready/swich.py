2f37e1aa0dffa8b5fe3664e9c2fd9e6086effbdc
권예경 <qwer4359@naver.com>
2025-02-12 17:47:56 +0900

    review plz

--- IMready/swich.py
+++ IMready/swich.py
@@ -3,24 +3,25 @@ sys.stdin = open('test.txt', 'r')
 
 T = int(input())
 
+def on_off(lst, start):
+    for i in range(start, len(lst)):
+        if lst[i] == 1:
+            lst[i] = 0
+        else:
+            lst[i] = 1
+    return lst
+
 for t in range(1,T+1):
     n = int(input())
-    ai = list(map(int,input().split()))
-    bi = list(map(int,input().split()))
+    ai = list(map(int, input().split()))
+    bi = list(map(int, input().split()))
 
-    i = n-1
-    count = 0
-    
-    while i > 0:
-        if ai[i] == bi[i]:
-            if ai[i-1] != bi[i-1]:
-                count += 1
-        else:
-            if ai[i-1] == bi[i-1]:
-                count += 1
-        i -= 1
-    
-    if ai != bi and count == 0:
-        count = 1
-        
-    print(f'# {t} {count}')
+    cnt = 0
+
+    for i in range(n):
+        if ai[i] != bi[i]:
+            ai = on_off(ai, i)
+            cnt += 1
+    if cnt == 0:
+        cnt = 2
+    print(f'#{t}',cnt)
--- sample.py
+++ sample.py
@@ -1,15 +1,43 @@
-lst = []
-for i in range(23):
-    data = input()
-    lst.append(data)
+t = 'TTTTTATTAATA'
+p = 'TTA'
 
-middle = ['inf', 'int', 'isf', 'ist', 'enf', 'ent', 'esf', 'est']
-count = [0] * 8
+def search(t,p):
+    N = len(t)
+    M = len(p)
+    for i in range(N-M+1):
+        for j in range(M):
+            if t[i+j] != p[j]:
+                break
+        else:
+            return i
+    return -1
 
-for data in lst:
-    for i in range(8):
-        if middle[i] in data:
-            count[i] += 1
-            break
+print(search(t,p))
 
-print(count)
+
+A = 'ABCD'
+B = 'EFGHIJKLMN'
+
+N= len(A)
+M = len(B)
+i = j = 0
+ans = ''
+while i+j < N+M:
+    if i <N:
+        ans += A[i]
+        i += 1
+    if j < M:
+        ans += B[j]
+        j+=1
+print(ans)
+
+i = j = 0
+ans = [0]*(N+M)
+while i+j < N+M:
+    if i < N:
+        ans[i+j] = A[i]
+        i+= 1
+    if j < M:
+        ans[i+j] = B[j]
+        j+=1
+print(''.join(ans))
