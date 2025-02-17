92e6eeb154b9cdc2648ca0265f226af26377b80f
권예경 <qwer4359@naver.com>
2025-02-17 11:35:52 +0900

    iron

--- IMready/iron_stick.py
+++ IMready/iron_stick.py
@@ -1,3 +1,23 @@
-
-sticks = list(input())
-
+import sys
+sys.stdin = open('test.txt')
+T = int(input())
+for t in range(1,T+1):
+    sticks = list(input())
+    stack = 0
+    i = 0
+    result = 0
+    while i < len(sticks) - 1:
+        if sticks[i] == '(':
+            if sticks[i+1] == ')':
+                result += stack
+                i += 2
+            else:
+                stack += 1
+                i += 1
+        else:
+            stack -= 1
+            result += 1
+            i += 1
+    if sticks[-2] != '(':
+        result += stack
+    print(f'#{t}',result)
